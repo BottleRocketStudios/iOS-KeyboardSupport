03599d8cc019c1a63d9d3cd4be543d716f0fc0a2
John Davis <john.davis@bottlerocketstudios.com>
2019-01-14 13:19:51 -0600

    Updates CGRect extension ACL and Adds comments.

--- Sources/KeyboardSupport/CGRect+Modifying.swift
+++ Sources/KeyboardSupport/CGRect+Modifying.swift
@@ -10,19 +10,37 @@ import CoreGraphics
 
 extension CGRect {
     
-    public func modifying(minX: CGFloat) -> CGRect {
+    
+    /// Returns a new CGRect instance with a modified minX property
+    ///
+    /// - Parameter minX: New value for minX
+    /// - Returns: New CGRect instance with new minX
+    func modifying(minX: CGFloat) -> CGRect {
         return CGRect(x: minX, y: minY, width: width, height: height)
     }
     
-    public func modifying(minY: CGFloat) -> CGRect {
+    
+    /// Returns a new CGRect instance with a modified minY property
+    ///
+    /// - Parameter minY: New value for minY
+    /// - Returns: New CGRect instance with new minY
+    func modifying(minY: CGFloat) -> CGRect {
         return CGRect(x: minX, y: minY, width: width, height: height)
     }
     
-    public func modifying(height: CGFloat) -> CGRect {
+    /// Returns a new CGRect instance with a modified height property
+    ///
+    /// - Parameter height: New value for height
+    /// - Returns: New CGRect instance with new height
+    func modifying(height: CGFloat) -> CGRect {
         return CGRect(x: minX, y: minY, width: width, height: height)
     }
     
-    public func modifying(width: CGFloat) -> CGRect {
+    /// Returns a new CGRect instance with a modified width property
+    ///
+    /// - Parameter width: New value for width
+    /// - Returns: New CGRect instance with new width
+    func modifying(width: CGFloat) -> CGRect {
         return CGRect(x: minX, y: minY, width: width, height: height)
     }
 }
