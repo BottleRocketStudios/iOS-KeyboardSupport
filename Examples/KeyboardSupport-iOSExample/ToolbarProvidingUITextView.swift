925d27dc65e30c2e016188b2b230d08e7cd2518b
John Davis <john.davis@bottlerocketstudios.com>
2019-01-22 09:56:47 -0600

    Adds a UITextView subclass that provides its own KeyboardAccessoryView to work around UIKit behavior.

---
+++ Examples/KeyboardSupport-iOSExample/ToolbarProvidingUITextView.swift
@@ -0,0 +1,31 @@
+//
+//  ToolbarProvidingUITextView.swift
+//  KeyboardSupport-iOS
+//
+//  Created by John Davis on 1/18/19.
+//  Copyright © 2019 Bottle Rocket. All rights reserved.
+//
+
+import UIKit
+
+/// UITextView, when used in with the `KeyboardAutoNavigator` has issues accepting it's toolbar being set in the UITextViewDidBeginEditing notification handler.
+/// To workaround this behavior, UITextViews need to set their own inputAccessoryView properties
+class ToolbarProvidingUITextView: UITextView {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let keyboardToolbar = KeyboardToolbar(frame: CGRect(x: 0, y: 0, width: 50.0, height: 44.0))
+        keyboardToolbar.addButton(type: .back, title: "Back")
+        keyboardToolbar.addButton(type: .next, title: "Next")
+        keyboardToolbar.addFlexibleSpace()
+        keyboardToolbar.addSystemDoneButton()
+        
+        inputAccessoryView = keyboardToolbar
+    }
+}
+
+extension ToolbarProvidingUITextView: KeyboardToolbarProviding {
+    var keyboardToolbar: KeyboardAccessoryView? {
+        return self.inputAccessoryView as? KeyboardToolbar
+    }
+}
