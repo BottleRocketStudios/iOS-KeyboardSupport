63e40ece1d63b6b06ea9fd7a0be9b96d94136fa6
John Davis <john.davis@bottlerocketstudios.com>
2019-02-11 14:55:25 -0600

    Updates KeyboardDismissable's returned GestureRecognizer to be discardable so there is no warning if the library user opts not to use it.

--- Sources/KeyboardSupport/KeyboardRespondable.swift
+++ Sources/KeyboardSupport/KeyboardRespondable.swift
@@ -27,10 +27,12 @@ public extension KeyboardRespondable where Self: UIViewController {
 /// Enables automatic keyboard dismissal via tapping the screen when the keyboard is displayed.
 public protocol KeyboardDismissable: class {
     /// Must be called once during setup ('viewDidLoad') to enable dismissal.  Returns gesture recognizer used for keyboard dismissal.
+    @discardableResult
     func setupKeyboardDismissalView() -> UIGestureRecognizer
 }
 
 public extension KeyboardDismissable where Self: UIViewController {
+    @discardableResult
     func setupKeyboardDismissalView() -> UIGestureRecognizer {
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(keyboardDismissalViewTapped))
         tapGestureRecognizer.cancelsTouchesInView = false
